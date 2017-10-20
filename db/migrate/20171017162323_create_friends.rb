cdb248312d604bdba5604fe0f5432afe16be322f
Vũ Minh Trí <1412579@student.hcmus.edu.vn>
2017-10-20 20:28:43 +0700

    update block/unblock user, fix add/delete friend. add new feature send message, to 1 or multi ppl.

--- db/migrate/20171017162323_create_friends.rb
+++ db/migrate/20171017162323_create_friends.rb
@@ -3,6 +3,7 @@ class CreateFriends < ActiveRecord::Migration[5.1]
     create_table :friends do |t|
       t.bigint :user_id
       t.bigint :friend_id
+      t.integer  :ban, :limit => 1, :default => 0
       t.timestamps
     end
   end
